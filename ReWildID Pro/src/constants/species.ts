cfb26f9d4ffc1849042fc571adee3d8e81ed14a0
abc123 <youguess@abc123.com>
2026-01-12 03:58:12 +1300

    add species list

--- ReWildID Pro/src/constants/species.ts
+++ ReWildID Pro/src/constants/species.ts
@@ -1,37 +1,17 @@
 // Currently supported species for ReID
-export const ACTIVE_SPECIES = ['stoat'] as const;
+export const ACTIVE_SPECIES = ['largemouth bass'] as const;
 
 // Species coming in future updates (alphabetical order)
 export const FUTURE_SPECIES = [
-    'bird',
-    'cat',
-    'deer',
-    'dog',
-    'ferret',
-    'goat',
-    'hedgehog',
-    'kea',
-    'kiwi',
-    'lagomorph',
-    'livestock',
-    'parakeet',
-    'pig',
-    'possum',
-    'pukeko',
-    'rodent',
-    'takahe',
-    'tomtit',
-    'tui',
-    'wallaby',
-    'weasel',
-    'weka',
-    'yellow eyed penguin'
+    'channel catfish',
+    'culter fish',
+    'redtail catfish'
 ] as const;
 
 // All species combined
 export const ALL_SPECIES = [...ACTIVE_SPECIES, ...FUTURE_SPECIES] as const;
 
-export const DEFAULT_SPECIES = 'stoat';
+export const DEFAULT_SPECIES = 'largemouth bass';
 
 export type ActiveSpecies = typeof ACTIVE_SPECIES[number];
 export type FutureSpecies = typeof FUTURE_SPECIES[number];
